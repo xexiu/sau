4a47f212791709eb34e77e9592a1cf685db42104
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-22 15:12:44 +0200

    Create function to add JSON elements

--- js/addBeach.js
+++ js/addBeach.js
@@ -1,29 +1,15 @@
-// var mydata = JSON.parse(data);
-// console.log(mydata[0].name);
+var beaches = JSON.parse(data);
+console.log(beaches[0].name);
 
-function loadJSON(callback) {
+var main = document.getElementById('main');
+// var section = document.getElementsByTagName('section');
+var section = document.createElement('section');
+section.id = 'important';
+var h1 = document.createElement('h1');
+var h1_content = document.createTextNode("Most important costs");
+var article = document.createElement('article')
+main.appendChild(section).appendChild(h1).appendChild(h1_content);
+main.appendChild(section).appendChild(article);
+// section.appendChild(article);
 
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'beaches.json', true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
 
-// .open will NOT return a value but simply returns undefined in async mode so use a callback
-callback(xobj.responseText);
-
-}
-}
-xobj.send(null);
-
-}
-
-// Call to function with anonymous callback
-loadJSON(function(response) {
-  //Do Something with the response e.g.
-  jsonresponse = JSON.parse(response);
-
-  //Assuming json data is wrapped in square brackets as Drew suggests
-  console.log(jsonresponse[0].name);
-
-});
