458fee4e7a2e357e68d2c5161a0973867dcda6da
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-25 15:19:02 +0200

    Fix scroll on sub-page

--- js/scroll_begur.js
+++ js/scroll_begur.js
@@ -12,47 +12,32 @@ $(document).ready(function() {
 $(window).scroll(function(){
   var pageY = pageYOffset;
   console.log(pageY);
-  if (pageY > 648 && pageY < 790 ) {
+  if (pageY > 619 && pageY < 760 ) {
     $('#tag_1').addClass('active');
   }else{
     $('#tag_1').removeClass('active');
   }
-  if (pageY > 790 && pageY < 980) {
+  if (pageY > 760 && pageY < 950) {
     $('#tag_2').addClass('active');
   }else{
     $('#tag_2').removeClass('active');
   }
+  if (pageY > 950 && pageY < 1140) {
+    $('#tag_3').addClass('active');
+  }else{
+    $('#tag_3').removeClass('active');
+  }
+  if (pageY > 1140 && pageY < 1295) {
+    $('#tag_4').addClass('active');
+  }else{
+    $('#tag_4').removeClass('active');
+  }
+  if (pageY > 1295 && pageY < 1485) {
+    $('#tag_5').addClass('active');
+  }else{
+    $('#tag_5').removeClass('active');
+  }
 });
-
-// if (pageY > 207 && pageY < 335) {
-//   $('#tag_2').addClass('active');
-// }else{
-//   $('#tag_2').removeClass('active');
-// }
-
-// if (pageY > 335 && pageY < 465) {
-//   $('#tag_3').addClass('active');
-// }else{
-//   $('#tag_3').removeClass('active');
-// }
-
-// if (pageY > 465 && pageY < 595) {
-//   $('#tag_4').addClass('active');
-// }else{
-//   $('#tag_4').removeClass('active');
-// }
-
-// if (pageY > 595 && pageY < 725) {
-//   $('#tag_5').addClass('active');
-// }else{
-//   $('#tag_5').removeClass('active');
-// }
-
-// if (pageY > 725 && pageY < 855) {
-//   $('#tag_6').addClass('active');
-// }else{
-//   $('#tag_6').removeClass('active');
-// }
 
 $("#tag_1").bind("click", function() {
   $('body').animate({
@@ -64,6 +49,27 @@ $("#tag_1").bind("click", function() {
 $("#tag_2").bind("click", function() {
   $('body').animate({
     scrollTop: $('.Whattodo').offset().top
+}, 1000); //scroll to div with container as ID.
+    return false; //Prevent Default and event bubbling.
+  });
+
+$("#tag_3").bind("click", function() {
+  $('body').animate({
+    scrollTop: $('.Activities').offset().top
+}, 1000); //scroll to div with container as ID.
+    return false; //Prevent Default and event bubbling.
+  });
+
+$("#tag_4").bind("click", function() {
+  $('body').animate({
+    scrollTop: $('.Restaurants').offset().top
+}, 1000); //scroll to div with container as ID.
+    return false; //Prevent Default and event bubbling.
+  });
+
+$("#tag_5").bind("click", function() {
+  $('body').animate({
+    scrollTop: $('.Nightlife').offset().top
 }, 1000); //scroll to div with container as ID.
     return false; //Prevent Default and event bubbling.
   });
